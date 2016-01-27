c6cbaa7dcbffc4b719f6af793c471ec3008dee9a
Ali <ilaimihar@gmail.com>
2016-01-27 22:02:09 +0330

    name var

--- temp.py
+++ temp.py
@@ -13,20 +13,20 @@ import imdb
 def find_ttl_by_name(name):
     url = "http://m.imdb.com/find?ref_=nv_sr_fn&s=tt&q=" + name
     page = requests.get(url)
-    
+
     link = ""
     soup = bs4.BeautifulSoup(page.content,"lxml")
     ttl = ""
     j = 0
     for s in soup.findAll('div'):
         if s.get('class') != None and s.get('class')[0] == 'title':
-            ttl = s.contents[1].get('href')    
+            ttl = s.contents[1].get('href')
             break
-        
-    return ttl
-    
 
-ttl = find_ttl_by_name("Toy Story 1".replace(" " , "+"))
+    return ttl
+
+movie_name = "Toy Story 1"
+ttl = find_ttl_by_name(movie_name.replace(" " , "+"))
 if len(ttl) > 0:
     res = imdb.find_detail_by_id(ttl.split("/")[2])
     print(res)
