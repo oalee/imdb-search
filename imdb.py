9d6e09f3f0b70c13eca56bbce2ff877f89aff23b
Ali <ilaimihar@gmail.com>
2016-01-28 12:35:22 +0330

    detail of movies in file is done

--- imdb.py
+++ imdb.py
@@ -5,6 +5,27 @@ import requests
 import lxml.html
 import bs4
 
+def find_ttl_by_name(name):
+    url = "http://m.imdb.com/find?ref_=nv_sr_fn&s=tt&q=" + name
+    page = requests.get(url)
+
+    link = ""
+    soup = bs4.BeautifulSoup(page.content,"lxml")
+    ttl = ""
+    j = 0
+    for s in soup.findAll('div'):
+        if s.get('class') != None and s.get('class')[0] == 'title':
+            ttl = s.contents[1].get('href')
+            break
+
+    return ttl
+
+def find_move_by_name(name):
+    ttl = find_ttl_by_name(name.replace(" " ,"+"))
+    if len(ttl) > 0:
+        ttl = ttl.split("/")[2]
+        return find_detail_by_id(ttl)
+
 def find_detail_by_id(id):
     hxs = lxml.html.document_fromstring(requests.get("http://imdb.com/title/" + id).content)
     movie = {}
@@ -16,7 +37,7 @@ def find_detail_by_id(id):
         movie['year'] = hxs.xpath('//h1')[0].text_content().split("\xa0")[1].replace(" ","").replace("(","").replace(")","")
     except IndexError:
         pass
-    
+
     try:
         movie['genre'] =[i.text_content() for i in hxs.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "subtext", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "itemprop", " " ))]')]
     except IndexError:
@@ -26,17 +47,17 @@ def find_detail_by_id(id):
     except IndexError:
         movie['rating'] = ""
     try:
-        movie['storyline'] = hxs.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "summary_text", " " ))]')[0].text_content()
+        movie['storyline'] = hxs.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "summary_text", " " ))]')[0].text_content().replace("                    ","").replace("            ","")
     except IndexError:
         movie['storyline'] = ""
     try:
         movie['metascore'] = hxs.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "titleReviewBarSubItem", " " ))]//span')[0].text_content()
     except IndexError:
-        movie['metascore'] = 0        
-        
-    return movie    
-        
-'''     
+        movie['metascore'] = 0
+
+    return movie
+
+'''
     try:
         movie['release_date'] = hxs.xpath('//*[@id="overview-top"]/div[2]/span[3]/a/text()')[0].strip()
     except IndexError:
@@ -44,7 +65,7 @@ def find_detail_by_id(id):
             movie['release_date'] = hxs.xpath('//*[@id="overview-top"]/div[2]/span[4]/a/text()')[0].strip()
         except Exception:
             movie['release_date'] = ""
-    
+
     try:
         movie['description'] = hxs.xpath('//*[@id="overview-top"]/p[2]/text()')[0].strip()
     except IndexError:
@@ -65,7 +86,7 @@ def find_detail_by_id(id):
         movie['gallery'] = hxs.xpath('//*[@id="combined-photos"]/div/a/img/@src')
     except IndexError:
         movie['gallery'] = ""
-  
+
     try:
         movie['votes'] = hxs.xpath('//*[@id="overview-top"]/div[3]/div[3]/a[1]/span/text()')[0].strip()
     except IndexError:
--- temp.py
+++ temp.py
@@ -5,28 +5,105 @@ Spyder Editor
 This is a temporary script file.
 """
 
-import bs4
-import requests
-import imdb
+from imdb import find_move_by_name
+from pathlib import Path
+import subprocess
+import logging
 
+logging.basicConfig(level= logging.INFO)
 
-def find_ttl_by_name(name):
-    url = "http://m.imdb.com/find?ref_=nv_sr_fn&s=tt&q=" + name
-    page = requests.get(url)
-
-    link = ""
-    soup = bs4.BeautifulSoup(page.content,"lxml")
-    ttl = ""
-    j = 0
-    for s in soup.findAll('div'):
-        if s.get('class') != None and s.get('class')[0] == 'title':
-            ttl = s.contents[1].get('href')
+def currectName(name):
+    name = name.lower()
+    cn = ""
+    qualities = ["720" , "1080" , "brip" , "bluray" , "dvd" ,"m-hd", "hd" , "web" , "brrip", "criterion"
+    , "blu-ray" , "director's cut" , "bdrip" , "x264" , "hdtv" , "xvid" , "ntsc" ]
+    for q in qualities:
+        if name.count(q) > 0:
+            cn = q
             break
+    if len(cn)> 0:
+        name = name.split(cn)[0]
+    forbidden_list = [ "-" , ")" , "(" , "]" , "[", "{" , "}"]
+    name = name.replace("." , " ").replace('_' , " ")
+    doReplace = False
+    temp = ""
+    for j in name:
+        if j == '[':
+            doReplace = True
 
-    return ttl
+        if not doReplace and j not in forbidden_list:
+            temp += j
+
+        if j == ']':
+            doReplace = False
+
+    name = temp.split(" ")
+    index = len(name)
+    while index > 0:
+        index = index - 1
+        try:
+            year = int(name[index])
+            if year > 1900 :
+                break
+        except:
+            pass
+    if index > 0 :
+        temp = ""
+        for k in range(0,index):
+            temp += name[k]
+            temp += " "
+    return temp
+
+def print_to_file(file,map):
+    file.write('title : ' + map['title'] + "\n")
+    file.write('rating : ' + map['rating']+ "\n")
+    file.write('metascore : ' + map['metascore']+ "\n")
+    file.write('year : ' + map['year']+ "\n")
+    file.write('genre : ' + str(map['genre'])+ "\n")
+    file.write('storyline : ' + map['storyline'])
+    file.write('dir : ' + map['dir'] + "\n")
+    file.write('size : ' + map['size']+ "\n")
+    file.write('-------------------------------------\n')
+
+
+def du(path):
+    """disk usage in human readable format (e.g. '2,1GB')"""
+    return subprocess.check_output(['du','-sh', path]).split()[0].decode('utf-8')
+
 
 movie_name = "Toy Story 1"
-ttl = find_ttl_by_name(movie_name.replace(" " , "+"))
-if len(ttl) > 0:
-    res = imdb.find_detail_by_id(ttl.split("/")[2])
-    print(res)
+#res = find_move_by_name(movie_name)
+
+
+dirs = ['/run/media/al/Data III/Movies' , '/run/media/al/Data/MoviE' ,'/run/media/al/Data/MoviE/2012', '/run/media/al/Data/MoviE/2011 Movies', '/run/media/al/Data/Movies' ,
+    '/run/media/al/Data II/Movies' , '/run/media/al/Data II/Movies_2' , '/run/media/al/Data II/Movies 2015' , '/run/media/al/Data IV/Movies']
+
+movies_list = []
+
+
+
+for dir in dirs:
+    movies_list += [(str(x).split("/")[-1] , str(x) , du(str(x))) for x in Path(dir).iterdir()]
+
+for item in movies_list:
+    logging.info(item)
+
+writer_file = open('my_movies','w')
+
+
+for i in range(0,2):
+    map = find_move_by_name(currectName(movies_list[i][0]))
+    if map == None:
+        continue
+    map['size'] =  str(movies_list[i][2])
+    map['dir'] = movies_list[i][1]
+    logging.info('got the detail of movie , map is ' + str(map))
+    print_to_file(writer_file , map)
+
+#print_to_file(writer_file , find_move_by_name(currectName(movie_name)))
+#print_to_file(writer_file ,find_move_by_name(currectName('sweeney todd')) )
+#writer_file.close()
+
+
+#for i in movies_list:
+#    writer_file.write(find_move_by_name(currectName(i[0])))
